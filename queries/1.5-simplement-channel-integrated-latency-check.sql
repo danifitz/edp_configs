5e8a03315030b02f3dc5f582c778539cc9b033a3
Daniel Fitzgerald <danielfitzgerald@newrelic.com>
2020-11-27 13:34:51 +0000

    updated simplement queries to reduce the amount of data returned per query, preventing timeouts

--- queries/1.5-simplement-channel-integrated-latency-check.sql
+++ queries/1.5-simplement-channel-integrated-latency-check.sql
@@ -1,5 +1,5 @@
 select  $__timeEpoch(vw_hvr_stats.last_updated) as event_timestamp, concat(chn_name,loc_name) as metric, metric_value
 from [HUBDB_PROD].[dbo].[vw_hvr_stats] vw_hvr_stats
 where metric='Integrate Latency Max'
-and  $__timeFilter(vw_hvr_stats.last_updated)
+and vw_hvr_stats.last_updated >= dateadd(minute, -10, getdate())
 order by last_updated;
--- queries/1.6-simplement-channel-capture-latency-check.sql
+++ queries/1.6-simplement-channel-capture-latency-check.sql
@@ -3,5 +3,5 @@ select  $__timeEpoch(last_updated),
      metric_value
      from [HUBDB_PROD].[dbo].[vw_hvr_stats]
      where metric='Capture Latency Max'
-      and  $__timeFilter(last_updated)
+      and  vw_hvr_stats.last_updated >= dateadd(minute, -10, getdate())
      order by last_updated
